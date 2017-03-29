e58d473993bb0049b005c0c8fc5c83ba948217ed
manmohan <manmohan.sharma@edgenetworks.in>
2017-03-29 17:32:02 +0530

    cahnges

--- birds/models/birds_model.py
+++ birds/models/birds_model.py
@@ -4,10 +4,10 @@ from mongoengine import DynamicDocument,connect,StringField,ListField,BooleanFie
 connect(db=config_dict["mongo"]["db_name"],host=config_dict["mongo"]["host"])
 
 class Birds(DynamicDocument):
-    id = StringField(required=True, primary_key=True) #Object id from the database
-    name = StringField(required=True)  #English bird name
-    family = StringField(required=True) #Latin bird family name
-    continents = ListField(StringField()) #Continents the bird exist on
+    id = StringField(required=True, primary_key=True,min_length=1) #Object id from the database
+    name = StringField(required=True,min_length=1)  #English bird name
+    family = StringField(required=True,min_length=1) #Latin bird family name
+    continents = ListField(StringField(),required=True,null=False) #Continents the bird exist on
     added = DateTimeField(required=True) #Date the bird was added to the registry. Format YYYY-MM-DD
     visible = BooleanField(default=False) #Determines if the bird should be visible in lists
     
